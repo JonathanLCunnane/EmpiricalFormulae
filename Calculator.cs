23a86a18f8e8e71b7fd80a28f691f1f28dcfff26
Jonathan Cunnane <jonathan.cunnane@gmail.com>
2022-07-01 10:08:37 +0100

    Formatted the empirical formula output using the Hill System standards.

--- Calculator.cs
+++ Calculator.cs
@@ -38,17 +38,43 @@ namespace EmpiricalFormulae
                 // If the maxVariance is at max of 0.02 then return the rounded 
                 if (maxVariance <= 0.02)
                 {
-                    string result = "";
-                    foreach (KeyValuePair<string, double> kvp in temp)
-                    {
-                        result += $"{kvp.Key}{SubscriptString((int)Math.Round(kvp.Value))} ";
-                    }
-                    return result;
+                    return FormatCompound(temp);
                 }
             }
             return "None found with 'maxVariance' of 0.02 within given computing time.";
         }
         
+        static string FormatCompound(Dictionary<string, double> elements)
+        {
+            // This function will format the compound based on the 'Hill Sytstem' or 'Hill Notation'
+            string soln = "";
+            if (elements.ContainsKey("C"))
+            {
+                soln += $"C{SubscriptString((int)Math.Round(elements["C"]))}";
+                elements.Remove("C");
+                if (elements.ContainsKey("H"))
+                {
+                    soln += $"H{SubscriptString((int)Math.Round(elements["H"]))}";
+                    elements.Remove("H");
+                }
+            }
+            // Add each element in alphabetical order if present in compound
+            foreach(string element in Consts.order)
+            {
+                if (elements.ContainsKey(element))
+                {
+                    soln += $"{element}{SubscriptString((int)Math.Round(elements[element]))}";
+                    elements.Remove(element);
+                }
+                // Break if there are no elements left
+                if (elements.Count == 0)
+                {
+                    break;
+                }
+            }
+            return soln;
+        }
+
         static string SubscriptString(int n)
         {
             string strn = n.ToString();
@@ -58,7 +84,6 @@ namespace EmpiricalFormulae
                 // Note that since this function will only be called from within this class that no 'try parsing' is needed before casting.
                 str += Consts.subscripts[int.Parse(strn[i].ToString())];
             }
-            Console.WriteLine($"{str}, {n}");
             return str;
         }
     }
